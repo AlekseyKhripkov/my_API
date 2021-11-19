0f7ea4046480cad762c1cca42b9d67b3b2203de3
Aleksey Khripkov <san4o3540479@icloud.com>
2021-11-19 20:23:25 +0300

    Создан контроллер

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class CurrencyController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @Route("/currency", name="get_currency", methods={"POST"})
+     */
+    public function getCurrency (Request $request)
+    {
+        $request = $request->toArray();
+        $result = [];
+
+    foreach ($request as $key => $value)
+    {
+        if ($key == "currency"){
+            if ($value = "USD"){
+                $result["name"] = "Доллар";
+                $result["code"] = "USD";
+            }
+        }
+
+        if ($key == "rateCurrency"){
+            if ($value == null || $value = "RUR"){
+                $result["rateCurrency"] = "RUR";
+                $result["rate"] = "70";
+            }
+            $result["rateCurrency"] = $value;
+        }
+
+        if ($key == "rateSum"){
+            if ($value == null || $value == 1){
+                $result["rateSum"] = "1";
+                $result["result"] = $result["rate"] * $result["rateSum"];
+            }
+            $result["rateSum"] = $value;
+            $result["result"] = $result["rate"] * $result["rateSum"];
+        }
+    }
+
+    return new JsonResponse(
+        ['name' => $result["name"],
+        'code' => $result["code"],
+        'result' => $result["result"]??null,
+        'rateCurrency' => $result["rateCurrency"],
+        'rateSum' => $result["rateCurrency"],
+        'rate' => $result["rate"]??null]
+    );
+    }
+
+}
