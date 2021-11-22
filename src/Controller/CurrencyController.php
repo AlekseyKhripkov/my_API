d0888815d44f839c7aac229b3e3985f7f38b5cb8
Aleksey Khripkov <san4o3540479@icloud.com>
2021-11-22 20:20:20 +0300

    Добавлены все валюты

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -15,7 +15,7 @@ class CurrencyController extends AbstractController
     public function getCurrency(Request $request)
     {
         $request = $request->toArray();
-        if (!array_key_exists("currency", $request)){
+        if (!array_key_exists("currency", $request)) {
             return new JsonResponse('currency является обязательным полем запроса');
         }
 
@@ -23,18 +23,23 @@ class CurrencyController extends AbstractController
         $result["rateCurrency"] = "RUR";
         $result["rateSum"] = 1;
         $requiredСurrency = null;
+        $date = date('Y-m-d');
+        $info = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js?"disclaimer"="https://www.cbr-xml-daily.ru/%23terms"&"date"="' . $date . '"&"rates"="' . $requiredСurrency . '"');
+        $info = json_decode($info, true);
 
         foreach ($request as $key => $value) {
             if ($key == "currency") {
                 if ($value == null) {
                     return new JsonResponse('Заполните необходимые поля');
-                } else if ($value == "USD") {
-                    $result["name"] = "Доллар";
-                    $result["code"] = "USD";
-                    $requiredСurrency = "USD";
-                }
-                else {
-                    return new JsonResponse('В currency введен неизвестный код валют (Возможено вы хотели ввести USD)');
+                } else {
+                    if (array_key_exists($value, $info["Valute"])) {
+                        $result["rate"] = $info["Valute"][$value]["Value"];
+                        $result["name"] = $info["Valute"][$value]["Name"];
+                        $result["code"] = $info["Valute"][$value]["CharCode"];
+                    } else {
+                        return new JsonResponse('В currency введен неизвестный код валют (Возможено вы хотели ввести USD)');
+                    }
+
                 }
             }
 
@@ -55,10 +60,6 @@ class CurrencyController extends AbstractController
             }
         }
 
-        $date = date('Y-m-d');
-        $info = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js?"disclaimer"="https://www.cbr-xml-daily.ru/%23terms"&"date"="'.$date.'"&"rates"="'.$requiredСurrency.'"');
-        $info = json_decode($info, true);
-        $result["rate"] = $info["Valute"]["USD"]["Value"];
         $result["result"] = $result["rate"] * $result["rateSum"];
 
         return new JsonResponse($result);
